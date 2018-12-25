d318d58a665ef702706255b60c7a29944a19f6bf
SamuelHenrique32 <32935669+SamuelHenrique32@users.noreply.github.com>
2018-12-25 16:59:11 -0200

    Aula 08 - Construtor e Encapsulamento

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,1 +1,3 @@
 let calculadora = new CalcController();             //poderia ser var
+
+//calculadora.displayCalc;                          //invoca o getter
--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,24 @@
-class CalcController{           //possui regras de negocio
+class CalcController{                 //possui regras de negocio
 
+    constructor(){                    //chamado automaticamente quando instancia
+
+        this._displayCalc = "0";      //display dos numeros, se fosse com var seria variavel, assim e atributo
+        this._dataAtual;              //underline e atributo privado  
+    }
+
+    get displayCalc(){                //getter method
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){           //setter method
+        this._displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
 }
