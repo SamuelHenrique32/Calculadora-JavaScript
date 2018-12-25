03d8217d0cb99d925c878e2d333493ae0a06ab69
SamuelHenrique32 <32935669+SamuelHenrique32@users.noreply.github.com>
2018-12-25 18:17:37 -0200

    DOM

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,3 @@
-let calculadora = new CalcController();             //poderia ser var
+let calculator = new CalcController();             //poderia ser var
 
 //calculadora.displayCalc;                          //invoca o getter
--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,7 +3,21 @@ class CalcController{                 //possui regras de negocio
     constructor(){                    //chamado automaticamente quando instancia
 
         this._displayCalc = "0";      //display dos numeros, se fosse com var seria variavel, assim e atributo
-        this._dataAtual;              //underline e atributo privado  
+        this._currentDate;              //underline e atributo privado  
+        this.initialize();
+    }
+
+    initialize(){
+        //seleciona elementos, amarra elemento à variável
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        //propriedade liberada com o DOM, pega objeto e coloca informacao dentro (no formato HTML)
+        //muda o HTML através de JavaScript
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/05/2018";
+        timeEl.innerHTML = "00:00";
     }
 
     get displayCalc(){                //getter method
@@ -14,11 +28,11 @@ class CalcController{                 //possui regras de negocio
         this._displayCalc = valor;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 }
